b62b8cdf487bcaaf92663aee5e0b4146771461f9
Artemiy <beljakovav@gmail.com>
2026-08-04 17:22:42 +0300

    feat: Добавить настройки соединения API Kafka 	modified:   internal/kafkaapi/methods.go

--- internal/kafkaapi/methods.go
+++ internal/kafkaapi/methods.go
@@ -2,6 +2,10 @@ package kafkaapi
 
 import (
 	"context"
+	"fmt"
+	"maps"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 // Start инициализирует новый модуль взаимодействия с API Kafka,
@@ -12,9 +16,42 @@ func (api *kafkaApiModule) Start(ctx context.Context) error {
 		return ctx.Err()
 	}
 
-	//
-	// сделать настроку для kafka с сертификатом и авторизацией
-	//
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": fmt.Sprintf("%s:%d", api.settings.host, api.settings.port),
+		"group.id":          fmt.Sprintf("%s-group", api.settings.nameRegionalObject), // Идентификатор группы
+		"auto.offset.reset": "earliest",                                               // Читать с начала
+	})
+	if err != nil {
+		return err
+	}
+	api.consumer = consumer
+	context.AfterFunc(ctx, func() {
+		consumer.Close()
+
+		close(api.chToModule)
+		close(api.chFromModule)
+	})
+
+	var topics []string
+	mapTopics := maps.Values(api.topics)
+	for topic := range mapTopics {
+		topics = append(topics, topic)
+	}
+
+	// подписка на топик
+	err = api.consumer.SubscribeTopics(topics, nil)
+	if err != nil {
+		return err
+	}
+
+	//обработчик подписок
+	go api.topicsHandler(ctx)
+
+	return nil
 }
 
 // GetChannelFromModule канал для приёма данных из модуля
@@ -25,4 +62,14 @@ func (api *kafkaApiModule) GetChannelFromModule() <-chan SettingsChanOutput {
 // GetChannelToModule канал для передачи данных в модуль
 func (api *kafkaApiModule) GetChannelToModule() chan<- SettingsChanInput {
 	return api.chToModule
+}
+
+// GetConsumer потребитель для kafka
+func (api *kafkaApiModule) GetConsumer() *kafka.Consumer {
+	return api.consumer
+}
+
+// GetTopics топики Kafka
+func (api *kafkaApiModule) GetTopics() map[string]string {
+	return api.topics
 }
